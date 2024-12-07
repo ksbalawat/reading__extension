bb25e564121cd9259019e389309393db2fadb7b9
ksbalawat <ksbalawat2002@gmail.com>
2024-12-07 23:24:45 +0530

    Converted client submodule into a regular folder

---
+++ client/components/MainPage.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+const MainPage = () => {
+    return (
+        <>
+        
+        <div className='columns-2 bg-gray-950 '>
+            <img  className='flex w-full h-full'src="https://www.21-draw.com/wp-content/uploads/2023/11/add-shadows-to-make-your-dog-drawing-more-realistic-e1700119847404.jpg" alt="" />
+            <img  className='fle w-full h-full'src="https://static-web.grammarly.com/1e6ajr2k4140/5A2EUIwGfqPMefCbN44eIK/18c44977a65fea654b32ed42517f2c21/Frame_2055245678__2_.png?w=1248&fm=webp" alt="" />
+
+        </div>
+        <br></br>
+        <br></br>
+        <div className="columns-5 bg-white-950" >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-20">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+        </svg>
+        <h1>
+            hello 
+        </h1>
+
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-20">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+            </svg>
+                <h1>
+                    hello
+                </h1>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0.7} stroke="currentColor" className="size-20">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" />
+            </svg>
+                <h1>
+                    hello
+                </h1>
+
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0.7} stroke="currentColor" className="size-20">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
+            </svg>
+                <h1>
+                    hello
+                </h1>
+
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0.7} stroke="currentColor" className="size-20">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+            </svg>
+
+                <h1>
+                    hello
+                </h1>
+            </div>
+
+            {/* <div className="bg-blue-500">
+                <div >
+
+                </div>
+
+            </div> */}
+
+<div className="bg-blue-500 text-white text-center p-6 rounded-lg h-40 w-full shadow-lg">
+  <h1 className="text-2xl font-bold mb-4 leading-relaxed">
+     Centered Heading Here
+  </h1>
+  <p className="text-base leading-7">
+    Your description goes here. It can span up to four lines or more depending on the content. 
+    Adjust the text to make it fit neatly within the block. 
+
+                
+  </p>
+
+  <div>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+
+
+<div className=" text-black text-center h-72 max-w-full">
+<h1 className="text-2xl font-bold mb-4 leading-relaxed">
+Get Ahead With Easify Assistance
+  </h1>
+  <p className="text-base leading-7">
+  Easify helps you generate drafts, polish punctuation, and work faster so you can spend less time
+  on small details and more time on big ideas.
+                
+  </p>
+</div>
+
+<div className="columns-2">
+    
+</div>
+
+<div className="flex items-center justify-between w-full p-8">
+  {/* Left Column (Image) */}
+  <div className="w-1/2 ">
+  <img  className='flex w-full h-full'src="https://www.21-draw.com/wp-content/uploads/2023/11/add-shadows-to-make-your-dog-drawing-more-realistic-e1700119847404.jpg" alt="" />
+  </div>
+
+  {/* Right Column (Text) */}
+  <div className="w-1/2 p-6">
+    <h2 className="text-3xl font-bold text-blue-600 mb-4"> Homepage Text Heading</h2>
+    <p className="text-lg text-gray-700 leading-relaxed">
+      Description goes heer and along with this u can aslo write the use cases 
+    </p>
+    <button className="mt-6 bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-400">Press me </button>
+  </div>
+</div> 
+
+
+<div className=" columns-2  ">
+            
+    <h2 className="text-3xl font-bold text-blue-600 "> Homepage Text Heading</h2>
+    <p className="text-lg text-gray-700 leading-relaxed">
+        
+        Description goes heer and along with this u can aslo write the use cases 
+    </p>
+    <button className="mt-6 bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-400">Press me </button>
+
+    <img  className='flex w-full h-full'src="https://www.21-draw.com/wp-content/uploads/2023/11/add-shadows-to-make-your-dog-drawing-more-realistic-e1700119847404.jpg" alt="" />
+
+
+    
+  
+    
+</div>
+
+
+
+</div>
+</div>
+
+
+        </>
+
+    )
+}
+
+export default MainPage
---
+++ client/pages/index.tsx
@@ -0,0 +1,14 @@
+import MainPage from "@/components/MainPage";
+import Landing from "../components/landing";
+
+export default function Home() {
+  return(
+    <div>
+      <Landing />;
+      <MainPage />;
+      
+  </div> 
+  )
+  
+
+}
